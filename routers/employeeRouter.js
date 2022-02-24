a5b967ebf1b6893ca27c4807d33fa5b6f9cff25c
Boaz <42014936+boazmenard@users.noreply.github.com>
2022-02-24 10:15:21 -0500

    add server listening port and begin router structure and files

---
+++ routers/employeeRouter.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const employeeRouter = express.Router()
+
+employeeRouter.get('/', (req, res, next) => {
+    
+})
+
+employeeRouter.post('/', (req, res, next) => {
+    
+})
+
+employeeRouter.get('/:employeeId', (req, res, next) => {
+
+})
+
+employeeRouter.put('/:employeeId', (req, res, next) => {
+    
+})
+
+employeeRouter.delete('/:employeeId', (req, res, next) => {
+    
+})
---
+++ routers/menuRouter.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const menuRouter = express.Router()
+
+menuRouter.get('/', (req, res, next) => {
+    
+})
+
+menuRouter.post('/', (req, res, next) => {
+    
+})
+
+menuRouter.get('/:menuId', (req, res, next) => {
+
+})
+
+menuRouter.put('/:menuId', (req, res, next) => {
+    
+})
+
+menuRouter.delete('/:menuId', (req, res, next) => {
+    
+})
