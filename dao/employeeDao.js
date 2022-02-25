626c1d84bbd1affb0665e63dfd82cba4469b743a
Boaz <42014936+boazmenard@users.noreply.github.com>
2022-02-25 14:25:56 -0500

    get only currently employed employees

--- dao/employeeDao.js
+++ dao/employeeDao.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
 const getAllEmployees = (req, res, next) => {
-    db.all('SELECT * FROM Employee', (err, rows) => {
+    db.all('SELECT * FROM Employee WHERE is_current_employee = 1', (err, rows) => {
         if (err) {
             console.log('Error has occured getting the employees')
             console.log(err.message)
